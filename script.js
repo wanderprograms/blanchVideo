71d1807b1b7ea55602a589ad9a1bec441dc813d7
wanderprograms <wanderslappprograms@gmail.com>
2026-01-01 03:46:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setTimeout(() => {
     if (splash) splash.remove(); // splash imachotsedwa kwathunthu
-  }, 6000); // masekondi 10
+  }, 8000); // masekondi 10
   // Replace with your Firebase config
   firebase.initializeApp({
     apiKey: "AIzaSyBBZxCwywnv_ZVXYezOV8IKG6iKWK5sL10",
